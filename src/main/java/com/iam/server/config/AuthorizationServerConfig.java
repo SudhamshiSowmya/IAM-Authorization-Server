d6395dbe5f86b47501d1444d59759fb32c9ed98a
Sudhamshi Sowmya <sudhamshisowmya@gmail.com>
2026-08-21 17:12:45 +0530

    Fix authorization server security configuration

--- src/main/java/com/iam/server/config/AuthorizationServerConfig.java
+++ src/main/java/com/iam/server/config/AuthorizationServerConfig.java
@@ -16,7 +16,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -29,23 +28,22 @@ public class AuthorizationServerConfig {
     public SecurityFilterChain authorizationServerSecurityFilterChain(
             HttpSecurity http) throws Exception {
 
-        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
-                OAuth2AuthorizationServerConfigurer.authorizationServer();
-
         http
-            .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
-            .with(authorizationServerConfigurer, authorizationServer ->
+            .oauth2AuthorizationServer(authorizationServer ->
                 authorizationServer
                     .oidc(Customizer.withDefaults())
             )
             .authorizeHttpRequests(authorize ->
-                authorize.anyRequest().authenticated()
+                authorize
+                    .anyRequest()
+                    .authenticated()
             )
             .exceptionHandling(exceptions ->
-                exceptions.defaultAuthenticationEntryPointFor(
-                    new LoginUrlAuthenticationEntryPoint("/login"),
-                    request -> true
-                )
+                exceptions
+                    .defaultAuthenticationEntryPointFor(
+                        new LoginUrlAuthenticationEntryPoint("/login"),
+                        request -> true
+                    )
             );
 
         return http.build();
